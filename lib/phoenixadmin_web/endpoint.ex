3335abb0cc8555b262fc2139c0eff965e9880feb
Arif <arif@get.com.mm>
2018-07-16 06:08:51 +0630

    fix endpoint to serve static file

--- lib/phoenixadmin_web/endpoint.ex
+++ lib/phoenixadmin_web/endpoint.ex
@@ -9,7 +9,7 @@ defmodule PhoenixadminWeb.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :phoenixadmin, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only_matching: ~w(css fonts images assets js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
